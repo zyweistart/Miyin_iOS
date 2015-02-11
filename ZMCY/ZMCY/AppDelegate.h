954be385194900db65a59160dd302e2bea5651d9
Start <4544242@qq.com>
2015-02-11 16:32:35 +0800

    1

---
+++ ZMCY/ZMCY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMCY
+//
+//  Created by Start on 2/11/15.
+//  Copyright (c) 2015 Start. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
