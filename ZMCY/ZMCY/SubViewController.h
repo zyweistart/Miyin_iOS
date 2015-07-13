23dd78040f5db13cbb502e225747c4773cde4503
Start <4544242@qq.com>
2015-07-13 17:10:57 +0800

    1

--- ZMCY/ZMCY/SubViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface SubViewController : UIViewController
-
-@property (nonatomic, strong) NSString *szSignal;
-
-- (id)initWithFrame:(CGRect)frame andSignal:(NSString *)szSignal;
-
-@end
