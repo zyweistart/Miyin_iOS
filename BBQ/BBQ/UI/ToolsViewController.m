6c3183734d4398701260462637498f07c5aa3270
Start <4544242@qq.com>
2015-08-05 15:44:09 +0800

    1

--- BBQ/BBQ/UI/ToolsViewController.m
+++ BBQ/BBQ/UI/ToolsViewController.m
@@ -171,21 +171,21 @@
     CGFloat height=[[Data Instance]mTabBarFrameViewController].view.bounds.size.height;
     [self.mChartItemLandView setCenter:CGPointMake(width/2,height/2)];
     if(currentZoomTag==1){
-        [self.mChartItemLandView loadData:self.mChartItemView1.currentData];
         [self.mChartItemLandView.lineChartView setIsAutoOffset:self.mChartItemView1.lineChartView.isAutoOffset];
         [self.mChartItemLandView.lineChartView setContentScroll:self.mChartItemView1.lineChartView.contentScroll];
+        [self.mChartItemLandView loadData:self.mChartItemView1.currentData];
     }else if(currentZoomTag==2){
-        [self.mChartItemLandView loadData:self.mChartItemView2.currentData];
         [self.mChartItemLandView.lineChartView setIsAutoOffset:self.mChartItemView2.lineChartView.isAutoOffset];
         [self.mChartItemLandView.lineChartView setContentScroll:self.mChartItemView2.lineChartView.contentScroll];
+        [self.mChartItemLandView loadData:self.mChartItemView2.currentData];
     }else if(currentZoomTag==3){
-        [self.mChartItemLandView loadData:self.mChartItemView3.currentData];
         [self.mChartItemLandView.lineChartView setIsAutoOffset:self.mChartItemView3.lineChartView.isAutoOffset];
         [self.mChartItemLandView.lineChartView setContentScroll:self.mChartItemView3.lineChartView.contentScroll];
+        [self.mChartItemLandView loadData:self.mChartItemView3.currentData];
     }else if(currentZoomTag==4){
-        [self.mChartItemLandView loadData:self.mChartItemView4.currentData];
         [self.mChartItemLandView.lineChartView setIsAutoOffset:self.mChartItemView4.lineChartView.isAutoOffset];
         [self.mChartItemLandView.lineChartView setContentScroll:self.mChartItemView4.lineChartView.contentScroll];
+        [self.mChartItemLandView loadData:self.mChartItemView4.currentData];
     }
     if([@"T1" isEqualToString:self.mChartItemLandView.currentKey]){
         self.mChartItemLandView.lineChartView.max=250;
